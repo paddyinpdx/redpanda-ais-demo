5b48358557105bf5bdfaf4c1f9f1ab06575a4b60
Patrick Barnes <pmbarnes@gmail.com>
2024-01-17 17:35:47 -0800

    fix: gracefully handle weather API errors

--- lib/consumer-and-producer-enrich-position-with-weather.py
+++ lib/consumer-and-producer-enrich-position-with-weather.py
@@ -20,7 +20,7 @@ producer_schema = "ship-position-event-with-weather"
 producer = utils.get_producer(producer_schema, logger)
 consumer = utils.get_consumer(group_id, consumer_topic, logger)
 
-logger.info(f'Starting kafka avro consumer loop, topic: {consumer_topic}. ^C to exit.')
+logger.info(f"Starting kafka avro consumer loop, topic: {consumer_topic}. ^C to exit.")
 
 try:
     consumer.subscribe([consumer_topic])
@@ -31,26 +31,33 @@ try:
             continue
 
         if msg.error():
-            logger.error(f'Error returned by poll: {msg.error()}')
+            logger.error(f"Error returned by poll: {msg.error()}")
         else:
             v = msg.value()
-            loc = v['location']
-            current_weather = json.loads(weather.get_current_weather_for_location(loc['lat'], loc['lon']))
-
-            wx_loc = current_weather['location']
-            wx = current_weather['current']
-            name =  wx_loc['name']
-            region = wx_loc['region']
-            country = wx_loc['country']
-            condition = wx['condition']['text']
-            temp = wx['temp_f']
-            wind_mph = wx['wind_mph']
-            wind_dir = wx['wind_dir']
-
-            logger.info(f'Current weather for {name}, {region}, {country}: {condition}, temp {temp}°F, wind {wind_mph}mph {wind_dir}')
-            logger.info(
-                f'offset {msg.offset()} key: {str(msg.key())} value: {str(v)}'
+            loc = v["location"]
+            current_weather = json.loads(
+                weather.get_current_weather_for_location(loc["lat"], loc["lon"])
             )
+            if "error" in current_weather:
+                logger.error(
+                    f'Error returned by weather API: {current_weather["error"]}'
+                )
+                continue
+
+            wx_loc = current_weather["location"]
+            wx = current_weather["current"]
+            name = wx_loc["name"]
+            region = wx_loc["region"]
+            country = wx_loc["country"]
+            condition = wx["condition"]["text"]
+            temp = wx["temp_f"]
+            wind_mph = wx["wind_mph"]
+            wind_dir = wx["wind_dir"]
+
+            logger.info(
+                f"Current weather for {name}, {region}, {country}: {condition}, temp {temp}°F, wind {wind_mph}mph {wind_dir}"
+            )
+            logger.info(f"offset {msg.offset()} key: {str(msg.key())} value: {str(v)}")
 
             consumer.commit()
 
@@ -71,13 +78,13 @@ try:
                 "condition": condition,
                 "temp_f": temp,
                 "wind_mph": wind_mph,
-                "wind_dir": wind_dir
+                "wind_dir": wind_dir,
             }
             utils.publish_message(producer, logger, producer_topic, key, value)
 except KeyboardInterrupt:
-    logger.info('Caught KeyboardInterrupt, stopping.')
+    logger.info("Caught KeyboardInterrupt, stopping.")
 except Exception as e:
-    logger.exception(f'Exception in consumer loop: {e}')
+    logger.exception(f"Exception in consumer loop: {e}")
     print("Handling the exception:", str(e))
 finally:
     if consumer is not None:
