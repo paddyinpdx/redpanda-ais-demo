c0aefecffb8afbe385fa21df191bc956e8cd3c88
Patrick Barnes <pmbarnes@gmail.com>
2024-02-02 18:54:03 -0800

    fix: add more API error handling

--- lib/consumer-and-producer-enrich-position-with-weather.py
+++ lib/consumer-and-producer-enrich-position-with-weather.py
@@ -45,7 +45,11 @@ try:
                 )
                 continue
             required_keys = ["location", "current"]
-
+            if not all(key in current_weather for key in required_keys):
+                logger.error(
+                    f"{current_weather}"
+                )
+                continue
             wx_loc = current_weather["location"]
             wx = current_weather["current"]
 
