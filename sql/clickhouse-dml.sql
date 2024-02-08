28ef27066acb64037efedeb5672daccf5ae2c82e
Patrick Barnes <pmbarnes@gmail.com>
2024-02-08 15:26:10 -0800

    fix: sql statement

--- sql/clickhouse-dml.sql
+++ sql/clickhouse-dml.sql
@@ -8,6 +8,7 @@ select * from system.kafka_consumers;
 --##########################################################################
 -- Join together the ship location and weather data with the ship metadata
 
-select mv.mmsi,t.name,t.callsign,t.type,t.destination,mv.status,mv.heading,mv.speed,mv.lat,mv.lon,mv.region,mv.locale,mv.condition,mv.temp_f,mv.wind_dir,mv.wind_mph,mv.timestamp
-from nst.ship_pos_and_wx_mv mv
-left outer join nst.ship_and_voyage t on mv.mmsi = t.mmsi
+select spw.mmsi,sid.shipname,sid.callsign,sid.shiptype,sid.destination,spw.status,spw.heading,spw.speed,spw.lat,spw.lon,spw.region,spw.locale,spw.condition,spw.temp_f,spw.wind_dir,spw.wind_mph,spw.timestamp
+from nst.ship_pos_and_wx_mv spw
+left outer join nst.ship_info_and_destination_mv sid on spw.mmsi = sid.mmsi
+where sid.shipname != ''
